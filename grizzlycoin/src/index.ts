0f816937756f5a19fa56fb1071fd6d7d3e509cd6
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-02-07 14:36:34 +0530

    :sparkles: add token metadata

--- grizzlycoin/src/index.ts
+++ grizzlycoin/src/index.ts
@@ -1,6 +1,18 @@
 import { initializeKeypair } from "./initializeKeypair";
 import * as web3 from "@solana/web3.js";
 import * as token from "@solana/spl-token";
+import {
+	Metaplex,
+	keypairIdentity,
+	bundlrStorage,
+	toMetaplexFile,
+} from "@metaplex-foundation/js";
+import {
+	DataV2,
+	createCreateMetadataAccountV2Instruction,
+	createUpdateMetadataAccountV2Instruction,
+} from "@metaplex-foundation/mpl-token-metadata";
+import * as fs from "fs";
 
 async function createMint(
 	connection: web3.Connection,
@@ -101,7 +113,147 @@ async function burnTokens(
 	mint: web3.PublicKey,
 	owner: web3.Keypair,
 	amount: number
-) {}
+) {
+	const mintInfo = await token.getMint(connection, mint);
+
+	const txnSignature = await token.burn(
+		connection,
+		payer,
+		account,
+		mint,
+		owner,
+		amount * 10 ** mintInfo.decimals
+	);
+
+	console.log(
+		`Burn Txn: https://explorer.solana.com/tx/${txnSignature}?cluster=devnet`
+	);
+}
+
+async function createTokenMetadata(
+	connection: web3.Connection,
+	metaplex: Metaplex,
+	mint: web3.PublicKey,
+	user: web3.Keypair,
+	name: string,
+	symbol: string,
+	description: string
+) {
+	const buffer = fs.readFileSync("assets/grizzly.svg");
+
+	const file = toMetaplexFile(buffer, "grizzly.svg");
+
+	const imageUri = await metaplex.storage().upload(file);
+	console.log(`Image URI: ${imageUri}`);
+
+	const { uri } = await metaplex.nfts().uploadMetadata({
+		name: name,
+		description: description,
+		image: imageUri,
+	});
+
+	console.log(`Metadata URI: ${uri}`);
+
+	const metadataPDA = metaplex.nfts().pdas().metadata({ mint });
+
+	const tokenMetadata = {
+		name: name,
+		symbol: symbol,
+		uri: uri,
+		sellerFeeBasisPoints: 0,
+		creators: null,
+		collection: null,
+		uses: null,
+	} as DataV2;
+
+	const txn = new web3.Transaction().add(
+		createCreateMetadataAccountV2Instruction(
+			{
+				metadata: metadataPDA,
+				mint: mint,
+				mintAuthority: user.publicKey,
+				payer: user.publicKey,
+				updateAuthority: user.publicKey,
+			},
+			{
+				createMetadataAccountArgsV2: {
+					data: tokenMetadata,
+					isMutable: true,
+				},
+			}
+		)
+	);
+
+	const txnSignature = await web3.sendAndConfirmTransaction(connection, txn, [
+		user,
+	]);
+
+	console.log(
+		`Create Metadata Account: https://explorer.solana.com/tx/${txnSignature}?cluster=devnet`
+	);
+}
+
+async function updateTokenMetadata(
+	connection: web3.Connection,
+	metaplex: Metaplex,
+	mint: web3.PublicKey,
+	user: web3.Keypair,
+	name: string,
+	symbol: string,
+	description: string
+) {
+	const buffer = fs.readFileSync("assets/grizzly.svg");
+
+	const file = toMetaplexFile(buffer, "grizzly.svg");
+
+	const imageUri = await metaplex.storage().upload(file);
+	console.log(`Image URI: ${imageUri}`);
+
+	const { uri } = await metaplex.nfts().uploadMetadata({
+		name: name,
+		description: description,
+		image: imageUri,
+	});
+
+	console.log(`Metadata URI: ${uri}`);
+
+	const metadataPDA = metaplex.nfts().pdas().metadata({ mint });
+
+	const tokenMetadata = {
+		name: name,
+		symbol: symbol,
+		uri: uri,
+		sellerFeeBasisPoints: 0,
+		creators: null,
+		collection: null,
+		uses: null,
+	} as DataV2;
+
+	const txn = new web3.Transaction().add(
+		createUpdateMetadataAccountV2Instruction(
+			{
+				metadata: metadataPDA,
+				updateAuthority: user.publicKey,
+			},
+			{
+				updateMetadataAccountArgsV2: {
+					data: tokenMetadata,
+					updateAuthority: user.publicKey,
+					primarySaleHappened: true,
+					isMutable: true,
+				},
+			}
+		)
+	);
+
+	const txnSignature = await web3.sendAndConfirmTransaction(connection, txn, [
+		user,
+	]);
+
+	console.log(
+		`Update Metadata Account: https://explorer.solana.com/tx/${txnSignature}?cluster=devnet`
+	);
+}
 
 async function main() {
 	const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
@@ -109,22 +261,65 @@ async function main() {
 
 	console.log("Public Key:", user.publicKey.toBase58());
 
-	const mint = await createMint(
+	const MINT_ADDRESS = "7TKtNeFJ59bQdD99eDzFNMbidQk2BVcg897a6dZkQQN7";
+
+	const metaplex = Metaplex.make(connection)
+		.use(keypairIdentity(user))
+		.use(
+			bundlrStorage({
+				address: "https://devnet.bundlr.network",
+				providerUrl: "https://api.devnet.solana.com",
+				timeout: 60000,
+			})
+		);
+
+	await createTokenMetadata(
 		connection,
+		metaplex,
+		new web3.PublicKey(MINT_ADDRESS),
 		user,
-		user.publicKey,
-		user.publicKey,
-		2
+		"Grizzly",
+		"GRZ",
+		"Thou shall take part in da Grizzlython"
 	);
 
-	const tokenAccount = await createTokenAccount(
-		connection,
-		user,
-		mint,
-		user.publicKey
-	);
+	// const mint = await createMint(
+	// 	connection,
+	// 	user,
+	// 	user.publicKey,
+	// 	user.publicKey,
+	// 	2
+	// );
 
-	await mintTokens(connection, user, mint, tokenAccount.address, user, 100);
+	// const tokenAccount = await createTokenAccount(
+	// 	connection,
+	// 	user,
+	// 	mint,
+	// 	user.publicKey
+	// );
+
+	// await mintTokens(connection, user, mint, tokenAccount.address, user, 100);
+
+	// const receiver = web3.Keypair.generate().publicKey;
+
+	// const receiverTokenAccount = await createTokenAccount(
+	// 	connection,
+	// 	user,
+	// 	mint,
+	// 	receiver
+	// );
+
+	// await transferTokens(
+	// 	connection,
+	// 	user,
+	// 	tokenAccount.address,
+	// 	receiverTokenAccount.address,
+	// 	user.publicKey,
+	// 	50,
+	// 	mint
+	// );
+
+	// await burnTokens(connection, user, tokenAccount.address, mint, user, 25);
 }
 
 main()
