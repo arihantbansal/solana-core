2fae5a3c85538395a814517d4adf1b33aaee0824
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-02-24 23:12:56 +0530

    :tada: init

---
+++ movie-review-comments/src/state.rs
@@ -0,0 +1,75 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    program_pack::{IsInitialized, Sealed},
+    pubkey::Pubkey,
+};
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieAccountState {
+    pub descriminator: String,
+    pub is_initialized: bool,
+    pub rating: u8,
+    pub title: String,
+    pub description: String,
+    pub reviewer: Pubkey,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieCommentCounter {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub counter: u64,
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieComment {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub review: Pubkey,
+    pub commenter: Pubkey,
+    pub comment: String,
+    pub count: u64,
+}
+
+impl MovieAccountState {
+    pub const DISCRIMINATOR: &'static str = "review";
+    pub fn get_account_size(title: String, description: String) -> usize {
+        return (4 + MovieAccountState::DISCRIMINATOR.len())
+            + 1
+            + 1
+            + (4 + title.len())
+            + (4 + description.len());
+    }
+}
+
+impl MovieComment {
+    pub const DISCRIMINATOR: &'static str = "comment";
+    pub fn get_account_size(comment: String) -> usize {
+        return (4 + MovieComment::DISCRIMINATOR.len()) + 1 + 32 + 32 + (4 + comment.len()) + 8;
+    }
+}
+
+impl MovieCommentCounter {
+    pub const DISCRIMINATOR: &'static str = "counter";
+    pub const SIZE: usize = (4 + MovieCommentCounter::DISCRIMINATOR.len()) + 1 + 8;
+}
+
+impl Sealed for MovieCommentCounter {}
+impl Sealed for MovieAccountState {}
+
+impl IsInitialized for MovieAccountState {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl IsInitialized for MovieCommentCounter {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+impl IsInitialized for MovieComment {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
