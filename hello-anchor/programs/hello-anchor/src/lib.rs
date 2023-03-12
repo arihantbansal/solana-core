ebf2b2b84821c2b2dee4b097e037d14b8dbebb55
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-03-12 12:36:39 +0530

    :sparkles: create counter program

--- hello-anchor/programs/hello-anchor/src/lib.rs
+++ hello-anchor/programs/hello-anchor/src/lib.rs
@@ -7,9 +7,41 @@ pub mod hello_anchor {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        msg!("Counter account created");
+        msg!("Current count: {}", counter.count);
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Update>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous Count: { }", counter.count);
+        counter.count = counter.count.checked_add(1).unwrap();
+        msg!("Counter Incremented");
+        msg!("Current Count: { }", counter.count);
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
