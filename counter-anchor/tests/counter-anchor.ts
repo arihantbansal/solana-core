c9fa34726a051c8e0f9315f19640cc7ff01a6750
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-03-12 13:59:23 +0530

    :white_check_mark: add tests

--- counter-anchor/tests/counter-anchor.ts
+++ counter-anchor/tests/counter-anchor.ts
@@ -1,16 +1,49 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import { expect } from "chai";
 import { CounterAnchor } from "../target/types/counter_anchor";
 
 describe("counter-anchor", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+	const provider = anchor.AnchorProvider.env();
 
-  const program = anchor.workspace.CounterAnchor as Program<CounterAnchor>;
+	anchor.setProvider(provider);
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+	const program = anchor.workspace.CounterAnchor as Program<CounterAnchor>;
+
+	const counter = anchor.web3.Keypair.generate();
+
+	it("Is initialized!", async () => {
+		const tx = await program.methods
+			.initialize()
+			.accounts({ counter: counter.publicKey })
+			.signers([counter])
+			.rpc();
+		console.log("Your transaction signature", tx);
+
+		const account = await program.account["counter"].fetch(counter.publicKey);
+		expect(account.count.toNumber() === 0);
+	});
+
+	it("Is incremented!", async () => {
+		const tx = await program.methods
+			.initialize()
+			.accounts({ counter: counter.publicKey, user: provider.wallet.publicKey })
+			.rpc();
+		console.log("Your transaction signature", tx);
+
+		const account = await program.account["counter"].fetch(counter.publicKey);
+		expect(account.count.toNumber() === 1);
+	});
+
+	it("Is decremented!", async () => {
+		const tx = await program.methods
+			.initialize()
+			.accounts({ counter: counter.publicKey, user: provider.wallet.publicKey })
+			.rpc();
+		console.log("Your transaction signature", tx);
+
+		const account = await program.account["counter"].fetch(counter.publicKey);
+		expect(account.count.toNumber() === 0);
+	});
 });
+
