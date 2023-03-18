db39ae322b66ef3c03cbdd557bd28775574de0f0
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-03-18 11:12:10 +0530

    :tada: begin anchor movie review

---
+++ anchor-movie-review/tests/anchor-movie-review.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { AnchorMovieReview } from "../target/types/anchor_movie_review";
+
+describe("anchor-movie-review", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.AnchorMovieReview as Program<AnchorMovieReview>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
